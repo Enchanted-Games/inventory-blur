2e9379b9a2a0e3d8bb4ff932e43c3f3a45af2527
ioblackshaw <isaacoblackshaw@gmail.com>
2026-06-20 21:42:32 +0100

    port to 26.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,7 @@ stonecutter {
         // use fabric_remap as the loader for obfuscated minecraft versions (1.21.11 or below)
 
         ver("26.1", "fabric", "neoforge")
+        ver("26.2", "fabric", "neoforge")
 
         vcsVersion = "26.1-fabric"
     }
